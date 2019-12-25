0266451489f31487ebcc26b67b90525dbf58a36d
Maja Tomic <majatomic98@hotmail.com>
2019-12-25 20:00:11 +0100

    #glavni_prozor

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,44 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import java.awt.HeadlessException;
+
+import java.awt.Toolkit;
+
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+
+
+
+
+public class MainFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public MainFrame() throws HeadlessException {
+		
+		Toolkit kit=Toolkit.getDefaultToolkit();
+		Dimension screenSize=kit.getScreenSize();
+		int screenHeight=screenSize.height;
+		int screenWidth=screenSize.width;
+		setSize(screenWidth*3/4,screenHeight*3/4);
+		setTitle("Studentska sluzba");
+		getContentPane().setBackground(Color.WHITE);
+		
+		setResizable(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		setIconImage(new ImageIcon("images/ftn_logo.jpg").getImage());
+
+		
+	}
+
+}
---
+++ src/main/MyApp.java
@@ -0,0 +1,14 @@
+package main;
+
+import gui.MainFrame;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				MainFrame mf=new MainFrame();
+				mf.setVisible(true);
+				
+	}
+
+}
