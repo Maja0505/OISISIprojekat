6638555e07ec0ffc05928ad0e251c40d139dee14
Nemanja Jevtic <njevtic998@gmail.com>
2019-12-26 13:44:04 +0100

    #status_bar

--- src/gui/StatusBar.java
+++ src/gui/StatusBar.java
@@ -1,13 +1,12 @@
 package gui;
 
 
+import java.awt.BorderLayout;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
-import javax.swing.border.BevelBorder;
 
 public class StatusBar extends JPanel {
 
@@ -17,21 +16,19 @@ public class StatusBar extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	public StatusBar() {
+		
 		super();
-		this.setBorder(new BevelBorder(BevelBorder.RAISED));
+		this.setLayout(new BorderLayout());
 		
-		JLabel statusBar = new JLabel("Studentska sluzba", SwingConstants.LEFT);
-		
+		JLabel statusBar = new JLabel("Studentska sluzba");
 		
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		LocalDateTime now = LocalDateTime.now();
-		
 		JLabel vreme = new JLabel(dtf.format(now));
 		
+		this.add(statusBar,BorderLayout.WEST);
 	
-		add(statusBar);
-		
-		add(vreme);
+		this.add(vreme,BorderLayout.EAST);
 		
 	}
 
