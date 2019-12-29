0204ba220eee36fa161d025a1ce6b5044ad47fc9
Maja Tomic <majatomic98@hotmail.com>
2019-12-29 23:18:12 +0100

    #predmeti_JTable

---
+++ src/tabele/PredmetiJTable.java
@@ -0,0 +1,47 @@
+package tabele;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumnModel;
+
+import listeners.MouseListener;
+
+public class PredmetiJTable extends JTable {
+	private static final long serialVersionUID = 1L;
+
+	public PredmetiJTable() {
+		super();
+		this.setRowHeight(30);
+		Font font = new Font("Serif",Font.BOLD,14);
+		this.setFont(font);
+		this.setBackground(Color.WHITE);
+		this.setForeground(Color.BLACK);
+		this.setRowSelectionAllowed(true);
+		this.setColumnSelectionAllowed(true);
+		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.setModel(new AbstractTableModelPredmeti());
+		this.setShowHorizontalLines(false);
+		
+        this.addMouseListener(new MouseListener(this));
+		
+	}
+	
+	@Override
+	public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+		Component c = super.prepareRenderer(renderer, row, column);
+		if (isRowSelected(row)) {
+			c.setBackground(Color.CYAN);
+		} else {
+			c.setBackground(Color.WHITE);
+		}
+		return c;
+	}
+
+	
+}
