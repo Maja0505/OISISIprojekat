f4c449c1fe75f6873b6ac65d2754e52a9107c59b
Maja Tomic <majatomic98@hotmail.com>
2019-12-30 18:49:44 +0100

    #ProfesoriJTable

---
+++ src/tabele/ProfesoriJTable.java
@@ -0,0 +1,49 @@
+package tabele;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.TableCellRenderer;
+import listeners.MouseListenerProfesori;
+
+public class ProfesoriJTable extends JTable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+	public ProfesoriJTable() {
+		super();
+		this.setRowHeight(30);
+		Font font = new Font("Serif",Font.BOLD,14);
+		this.setFont(font);
+		this.setBackground(Color.WHITE);
+		this.setForeground(Color.BLACK);
+		this.setRowSelectionAllowed(true);
+		this.setColumnSelectionAllowed(true);
+		this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.setModel(new AbstractTableModelProfesori());
+		this.setShowHorizontalLines(false);
+		this.setAutoCreateRowSorter(true);
+		
+        this.addMouseListener(new MouseListenerProfesori(this));
+	}
+		
+	
+	@Override
+	public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+		Component c = super.prepareRenderer(renderer, row, column);
+		if (isRowSelected(row)) {
+			c.setBackground(Color.CYAN);
+		} else {
+			c.setBackground(Color.WHITE);
+		}
+		return c;
+	}
+
+}
