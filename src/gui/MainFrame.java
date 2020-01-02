36580f160ca3af64dfd7c74c8f22c0a2fa36b4a3
Maja Tomic <majatomic98@hotmail.com>
2020-01-02 23:05:26 +0100

    #dorada_MainFrame

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -22,6 +22,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import tabele.AbstractTableModelPredmeti;
 import tabele.AbstractTableModelStudenti;
 import tabele.PredmetiJTable;
 import tabele.ProfesoriJTable;
@@ -39,6 +40,20 @@ public class MainFrame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private static MainFrame instance = null;
+
+	private JTable tabelaStudenata;
+	private JTable tabelaPredmeta;
+	private JTable tabelaProfesora;
+	public static int tab;  // da bi znali koji tab je aktivan
+	
+	public static MainFrame getInstance() {
+		if (instance == null) {
+			instance = new MainFrame();
+		}
+		return instance;
+	}
 
 	public MainFrame() throws HeadlessException {
 		
@@ -78,8 +93,7 @@ public class MainFrame extends JFrame {
 		setVisible(true);
 	}
 	
-	public static int tab;  // da bi znali koji tab je aktivan
-
+	
 	public void inicijalizacijaTabbedPane() {
 		 JTabbedPane tabbedPane = new JTabbedPane();
 		 JPanel panel1, panel2, panel3;
@@ -91,70 +105,62 @@ public class MainFrame extends JFrame {
 	      tabbedPane.addTab("PROFESORI ", panel2);
 	      tabbedPane.addTab("PREDMETI", panel3);
 	      
-	      tabelaStudenata = new StudentiJTable();
-			JScrollPane scrollPane = new JScrollPane(tabelaStudenata);
-			scrollPane.setPreferredSize(new Dimension(1300, 1500));
-			scrollPane.setBorder(new EmptyBorder(60, 50, 30, 40));
-			
+	     tabelaStudenata = new StudentiJTable();
+				JScrollPane scrollPane = new JScrollPane(tabelaStudenata);
+				scrollPane.setPreferredSize(new Dimension(1300, 1500));
+				scrollPane.setBorder(new EmptyBorder(60, 50, 30, 40));
+				
 		 tabelaPredmeta = new PredmetiJTable();
 				JScrollPane scrollPane1 = new JScrollPane(tabelaPredmeta);
 				scrollPane1.setPreferredSize(new Dimension(1300, 1500));
 				scrollPane1.setBorder(new EmptyBorder(60, 50, 30, 40));
 				
-		tabelaProfesora=new ProfesoriJTable();		
+		 tabelaProfesora=new ProfesoriJTable();		
 				JScrollPane scrollPane2 = new JScrollPane(tabelaProfesora);
 				scrollPane2.setPreferredSize(new Dimension(1300, 1500));
 				scrollPane2.setBorder(new EmptyBorder(60, 50, 30, 40));
 			
 				
-			panel1.add(scrollPane, BorderLayout.SOUTH);
-			panel2.add(scrollPane2,BorderLayout.SOUTH);
-			panel3.add(scrollPane1, BorderLayout.SOUTH);
+	   	panel1.add(scrollPane, BorderLayout.SOUTH);
+		panel2.add(scrollPane2,BorderLayout.SOUTH);
+		panel3.add(scrollPane1, BorderLayout.SOUTH);
 			
-	      Font font = new Font("Serif", Font.BOLD, 20); 
-	      tabbedPane.setFont(font);
-	      tabbedPane.setForeground(Color.WHITE);
-	      tabbedPane.setBackground(Color.GRAY);
+	    Font font = new Font("Serif", Font.BOLD, 20); 
+	    tabbedPane.setFont(font);
+	    tabbedPane.setForeground(Color.WHITE);
+	    tabbedPane.setBackground(Color.GRAY);
 	      
-//reaguje na promenu taba	
+	      //reaguje na promenu taba	
 	      tabbedPane.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				JTabbedPane sourceJTabbedPane = (JTabbedPane) e.getSource();
-				tab = sourceJTabbedPane.getSelectedIndex();
-			}
-		});
+				@Override
+				public void stateChanged(ChangeEvent e) {
+					JTabbedPane sourceJTabbedPane = (JTabbedPane) e.getSource();
+					tab = sourceJTabbedPane.getSelectedIndex();
+				}
+	      	});
 	      	
 	      
-	    	this.add(tabbedPane);
+	   	this.add(tabbedPane);
 	 
 	}
 
 	
-
-	private static MainFrame instance = null;
-
-	public static MainFrame getInstance() {
-		if (instance == null) {
-			instance = new MainFrame();
-		}
-		return instance;
-	}
-
-	private JTable tabelaStudenata;
-	private JTable tabelaPredmeta;
-	private JTable tabelaProfesora;
-	
-		
-//metoda da azuriranje modela Student 
+	//metoda da azuriranje modela Student 
 	public void azurirajTabeluStudent() {
 			AbstractTableModelStudenti model = (AbstractTableModelStudenti) tabelaStudenata.getModel();
 			model.fireTableDataChanged();
 			validate();
-	}	
+	}
+	//metoda da azuriranje modela Predmeta 	
+	public void azurirajTabeluPredmeta() {
+		AbstractTableModelPredmeti model = (AbstractTableModelPredmeti) tabelaPredmeta.getModel();
+		model.fireTableDataChanged();
+		validate();
+	}		
 	
 	private void prikaziTabele() {
 		this.azurirajTabeluStudent();
+		this.azurirajTabeluPredmeta();
 	}
 	
 	
