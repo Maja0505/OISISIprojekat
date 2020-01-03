225eccd9286e9d92f15d3665af2e76371d2d932b
Maja Tomic <majatomic98@hotmail.com>
2020-01-03 11:20:02 +0100

    #dorada_PredmetiController

--- src/controlleri/PredmetiController.java
+++ src/controlleri/PredmetiController.java
@@ -4,6 +4,7 @@ import gui.MainFrame;
 import modeli.BazaPredmeta;
 import modeli.Predmet;
 
+
 public class PredmetiController {
 	
 private static PredmetiController instance = null;
@@ -33,5 +34,18 @@ private static PredmetiController instance = null;
 	public void dodajPredmet(Predmet p) {
 			BazaPredmeta.getInstance().dodajPredmet(p);
 			MainFrame.getInstance().azurirajTabeluPredmeta();
+	}
+	
+	public void izmeniPredmet(Predmet p) {
+		/*if (rowSelectedIndex < 0) {
+			return;
+		}
+		// izmena modela
+		Predmet predmet = BazaPredmeta.getInstance().getRow(rowSelectedIndex);
+		*/
+		BazaPredmeta.getInstance().izmeniPredmet(p.getSifraPredmeta(), p.getNazivPredmet(),p.getSemestar(),p.getGodinaIzvodjenjaPredmeta(),p.getPredmetniProfesor());
+	
+		// azuriranje prikaza
+		MainFrame.getInstance().azurirajTabeluPredmeta();
 	}
 }
