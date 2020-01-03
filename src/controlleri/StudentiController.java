8167b89adbb15dd68b79f42f002559ff5c85b2f3
Nemanja Jevtic <njevtic998@gmail.com>
2020-01-03 13:39:13 +0100

    #dorada_StudentiController

--- src/controlleri/StudentiController.java
+++ src/controlleri/StudentiController.java
@@ -23,9 +23,29 @@ private static StudentiController instance = null;
 		BazaStudenata.getInstance().dodajStudenta(s);
 		//azuriranje prikaza
 		MainFrame.getInstance().azurirajTabeluStudent();
-		
 	}
 	
+	
+	public void izbrisiStudenta(int rowSelectedIndex) {
+    	if (rowSelectedIndex < 0) {
+			return;
+		}
+    	// izmena modela Student (brisanje)
+    	Student student = BazaStudenata.getInstance().getRow(rowSelectedIndex);
+		BazaStudenata.getInstance().izbrisiStudenta(student.getBrIndeksa());
+		// azuriranje prikaza
+		MainFrame.getInstance().azurirajTabeluStudent();;
+    }
+	
+	
+	public void izmeniStudenta(Student s) {
+		// izmena modela Student (izmena)
+		BazaStudenata.getInstance().izmeniStudenta(s.getIme(), s.getPrezime(), s.getDatumRodjenja(),
+				s.getAdresa(), s.getBrojTelefona(), s.getEmail(), s.getBrIndeksa(), s.getGodinaUpisa(),s.getTrenutnaGodinaStudija(),
+				s.getProsecnaOcena(), s.getSpisakPredmeta(), s.getStatus());
+		// azuriranje prikaza
+		MainFrame.getInstance().azurirajTabeluStudent();
+	}
 	
 	
 }
