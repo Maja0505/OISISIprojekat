065a3ffa7e6b986e54c0f93f855b3f92d516f3dc
Nemanja Jevtic <njevtic998@gmail.com>
2020-01-03 13:41:58 +0100

    #dorada_izmena_Studenta

--- src/dialozi/DijalogIzmenaStudenta.java
+++ src/dialozi/DijalogIzmenaStudenta.java
@@ -6,6 +6,13 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -13,26 +20,38 @@ import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
+import controlleri.StudentiController;
+import listeners.FocusListener;
+import listeners.KeyListener;
+import modeli.BazaStudenata;
+import modeli.Student;
+import modeli.Student.Status;
+import tabele.StudentiJTable;
+
+
 public class DijalogIzmenaStudenta {
 
+	private static int onemoguciTxtField = 0;
+	
 	public DijalogIzmenaStudenta(boolean vidljiv) {
-		JDialog  dodajStudenta = new ModalniDijalog(new JFrame(), "Izmeni studenta", true,600,750);
+JDialog  izmeniStudenta = new ModalniDijalog(new JFrame(), "Dodaj studenta", true,600,750);
 		
 		
 		JLabel statusBar = new JLabel();
-		dodajStudenta.add(statusBar,BorderLayout.SOUTH);
-		statusBar.setPreferredSize(new Dimension(dodajStudenta.getWidth(), 25));
+		izmeniStudenta.add(statusBar,BorderLayout.SOUTH);
+		statusBar.setPreferredSize(new Dimension(izmeniStudenta.getWidth(), 25));
 		
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
 		panel.setBackground(Color.WHITE);
-		dodajStudenta.add(panel,BorderLayout.CENTER);
+		izmeniStudenta.add(panel,BorderLayout.CENTER);
 		
-		dodajStudenta.add(panel);
+		izmeniStudenta.add(panel);
 		
 		JLabel ime = new JLabel("Ime*");
 		JLabel prz = new JLabel("Prezime*");
@@ -45,42 +64,214 @@ public class DijalogIzmenaStudenta {
 		JLabel prosecnaOcena = new JLabel("Prosecna ocena* ");
 		JLabel godUpis = new JLabel("Godina upisa* ");
 		
-		JTextField Ime = new JTextField();
+		JTextField Ime = new JTextField(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getIme());
+		Ime.setName("Ime*");
 		Ime.setPreferredSize(new Dimension(100,30));
-		JTextField Prz = new JTextField();
+		Ime.addFocusListener(new FocusListener());
+		
+		JTextField Prz = new JTextField(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getPrezime());
+		Prz.setName("Prz*");
 		Prz.setPreferredSize(new Dimension(100,30));
-		JTextField Dat = new JTextField();
+		Prz.addFocusListener(new FocusListener());
+		
+		JTextField Dat = new JTextField(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getDatumRodjenja().toString());
+		Dat.setName("Dat*");
 		Dat.setPreferredSize(new Dimension(100,30));
-		JTextField Adresa = new JTextField();
+		Dat.addFocusListener(new FocusListener());
+		
+		JTextField Adresa = new JTextField(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getAdresa());
+		Adresa.setName("Adresa*");
 		Adresa.setPreferredSize(new Dimension(100,30));
-		JTextField Broj = new JTextField();
+		Adresa.addFocusListener(new FocusListener());
+		
+		JTextField Broj = new JTextField(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getBrojTelefona());
+		Broj.setName("Broj*");
 		Broj.setPreferredSize(new Dimension(100,30));
-		JTextField Indeks = new JTextField();
+		Broj.addKeyListener(new KeyListener());
+		Broj.addFocusListener(new FocusListener());
+		
+		JTextField Indeks = new JTextField(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getBrIndeksa());
+		Indeks.setName("Indeks*");
 		Indeks.setPreferredSize(new Dimension(100,30));
-		JTextField Email = new JTextField();
+		Indeks.addFocusListener(new FocusListener());
+		
+		JTextField Email = new JTextField(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getEmail());
+		Email.setName("Email*");
 		Email.setPreferredSize(new Dimension(100,30));
-		JTextField ProsecnaOcena = new JTextField();
+		Email.addFocusListener(new FocusListener());
+		
+		JTextField ProsecnaOcena = new JTextField(String.valueOf(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getProsecnaOcena()));
+		ProsecnaOcena.setName("Prosecna ocena*");
 		ProsecnaOcena.setPreferredSize(new Dimension(100,30));
-		JTextField GodUpis = new JTextField();
+		ProsecnaOcena.addFocusListener(new FocusListener());
+		
+		JTextField GodUpis = new JTextField(String.valueOf(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getGodinaUpisa()));
+		GodUpis.setName("GodUpis*");
 		GodUpis.setPreferredSize(new Dimension(100,30));
+		GodUpis.addFocusListener(new FocusListener());
+		
+		List<String> predmeti = new ArrayList<String>();
+		Student student = new Student();
+		
+		student.setSpisakPredmeta(predmeti);
+		
 		
 	    String[] izbor = { "I (prva)","II (druga) ", "III (treca)","IV (cetvrta)"};
 
 	    final JComboBox<String> cb = new JComboBox<String>(izbor);
 	    cb.setPreferredSize(new Dimension(100,30));
-		
+	    String godina = cb.getItemAt(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getTrenutnaGodinaStudija() - 1);
+	    cb.setSelectedItem(godina);
+	    
+	    
+	    cb.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int godStud = cb.getSelectedIndex() + 1; 
+				student.setTrenutnaGodinaStudija(godStud);
+			}
+		});
+	    
+	    if(student.getGodinaUpisa() == 0)
+	    	student.setTrenutnaGodinaStudija(1); 
 	    
 	    JRadioButton button1 = new JRadioButton("Budzet");
 	    JRadioButton button2 = new JRadioButton("Samofinansiranje");
 	    ButtonGroup buttonGroup = new ButtonGroup();
+	    button1.setBackground(Color.WHITE);
+	    button2.setBackground(Color.WHITE);
+	   
+	    if(String.valueOf(BazaStudenata.getInstance().getStudenti().get(StudentiJTable.selektovanaVrsta).getStatus()) == "B") {
+	    	button1.setSelected(true);
+	    	student.setStatus(Status.B);
+	    	}else {
+	    	button2.setSelected(true);
+	    	student.setStatus(Status.S);
+	    }
 	    
 	    buttonGroup.add(button1);
 	    buttonGroup.add(button2);
 	    
-	    JButton odustanak = new JButton("Odustanak");
-	    JButton potvrda = new JButton("Potvrda");
 	    
+	    
+	   button1.addActionListener(new ActionListener() {
 		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(button1.isSelected()) {
+				student.setStatus(Status.B);
+			}
+		}
+	});
+	   
+	   button2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(button2.isSelected()) {
+					student.setStatus(Status.S);
+				}
+				}
+		});
+	   
+	   
+	   
+	   
+	    
+	    JButton odustanak = new JButton("Odustanak");
+	    odustanak.setBackground(Color.LIGHT_GRAY);
+	    JButton potvrda = new JButton("Potvrda");
+	    potvrda.setBackground(Color.CYAN);
+	    
+	    potvrda.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			
+				
+				
+				if(!Ime.getText().equals("")  && !Ime.getText().equals("Nije validan unos imena...")) {
+					onemoguciTxtField++;
+					}
+				if(!Prz.getText().equals("")  && !Prz.getText().equals("Nije validan unos prezimena...")) {
+					onemoguciTxtField++;
+					}
+				if(!Dat.getText().equals("") && !Dat.getText().equals("Nije validan format datuma (ocekivani 'YYYY - MM - DD')...")) {
+					onemoguciTxtField++;
+					}
+				if(!Adresa.getText().equals("") && !Adresa.getText().equals("Nije valian unos adrese...")) {
+					onemoguciTxtField++;
+					}
+				if(!Broj.getText().equals("") && !Broj.getText().equals("Validan format je +381*********...")) {
+					onemoguciTxtField++;
+					}
+				if(!Email.getText().equals("") && !Email.getText().equals("Nije validan format za email...")) {
+					onemoguciTxtField++;
+					}
+				if(!Indeks.getText().equals("") && !Indeks.getText().equals("Validan format je xxBROJ-20YY...")) {
+					onemoguciTxtField++;
+				}
+				if(!ProsecnaOcena.getText().equals("") && !ProsecnaOcena.getText().equals("Ocene su u rasponu od 6 - 10...")) {
+					onemoguciTxtField++;
+					}
+				if(!GodUpis.getText().equals("") && !GodUpis.getText().equals("Nije validan unos...")) {
+					onemoguciTxtField++;
+					}
+				
+				System.out.println(onemoguciTxtField);
+			if(onemoguciTxtField == 9) {
+				String ime = Ime.getText();
+				student.setIme(ime);
+				String prz = Prz.getText();
+				student.setPrezime(prz);
+				String dat = Dat.getText();
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");
+				try {
+					Date god = new Date(sdf.parse(dat).getTime());
+					student.setDatumRodjenja(god);
+				} catch (ParseException e1) {
+					e1.printStackTrace();
+				}
+				String adresa = Adresa.getText();
+				student.setAdresa(adresa);
+				String broj = Broj.getText();
+				student.setBrojTelefona(broj);
+				String email = Email.getText();
+				student.setEmail(email);
+				String indeks = Indeks.getText();
+				student.setBrIndeksa(indeks);
+				String prosecnaOcena = ProsecnaOcena.getText();
+				float prosecna = Float.parseFloat(prosecnaOcena);
+				student.setProsecnaOcena(prosecna);
+				String godString = GodUpis.getText();
+				int godUpis = Integer.parseInt(godString);
+				student.setGodinaUpisa(godUpis);
+				
+				
+				StudentiController.getInstance().izmeniStudenta(student);
+				onemoguciTxtField = 0;
+				izmeniStudenta.dispose();
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Niste uneli sva polja");
+			    onemoguciTxtField = 0;
+				}
+	
+			}
+	
+	    });
+	    
+	    
+	    odustanak.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				onemoguciTxtField = 0;
+				izmeniStudenta.dispose();
+			}
+		});
+		
+	    
 		GridBagConstraints gbc = new GridBagConstraints();
 		
 		
@@ -191,7 +382,7 @@ public class DijalogIzmenaStudenta {
 		gbc.insets = new Insets(0, 170, 0, 150);
 		panel.add(odustanak, gbc);
 		
-		dodajStudenta.setVisible(vidljiv);
+		izmeniStudenta.setVisible(vidljiv);
 	}
 
 	
