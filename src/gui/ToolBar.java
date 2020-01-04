40a15e810c6524845e9a47d1ad1cceb29941fc6e
Maja Tomic <majatomic98@hotmail.com>
2020-01-04 19:06:48 +0100

    #pretraga_predmeta

--- src/gui/ToolBar.java
+++ src/gui/ToolBar.java
@@ -1,24 +1,23 @@
 package gui;
 
 
-
-
-
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
+import javax.swing.RowFilter;
 import javax.swing.SwingConstants;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
-
 import dialozi.DijalogBrisanjePredmeta;
 import dialozi.DijalogBrisanjeProfesora;
 import dialozi.DijalogBrisanjeStudenta;
@@ -28,6 +27,7 @@ import dialozi.DijalogDodajStudenta;
 import dialozi.DijalogIzmenaPredmeta;
 import dialozi.DijalogIzmenaProfesora;
 import dialozi.DijalogIzmenaStudenta;
+import tabele.AbstractTableModelPredmeti;
 import tabele.PredmetiJTable;
 import tabele.ProfesoriJTable;
 import tabele.StudentiJTable;
@@ -114,13 +114,28 @@ public class ToolBar extends JToolBar {
 						}
 					});
 		
+		addSeparator();
 		
-		for(int i =0;i<90;i++) {
+		JButton studentButton=new JButton();
+		studentButton.setToolTipText("Dodavanje studenta na predmet");
+		studentButton.setIcon(new ImageIcon("images/add.jpg"));
+		
+		add(studentButton);
+		
+		addSeparator();
+		
+		JButton profesorButton=new JButton();
+		profesorButton.setToolTipText("Dodavanje profesora na predmet");
+		profesorButton.setIcon(new ImageIcon("images/profesor.jpg"));
+		
+		add(profesorButton);
+		
+		for(int i =0;i<85;i++) {
 		addSeparator();
 		}
 
 		JTextField textField=new JTextField();
-		textField.setDocument(new JTextFieldLimit(80));
+		textField.setDocument(new JTextFieldLimit(100));
 		 Font font = new Font("Serif", Font.BOLD, 15); 			//font za unos
 		 textField.setFont(font);
 		 add(textField);
@@ -131,6 +146,88 @@ public class ToolBar extends JToolBar {
 		JButton buttonSearch=new JButton();
 		buttonSearch.setToolTipText("Pretraga");
 		buttonSearch.setIcon(new ImageIcon("images/pretraga.jpg"));
+		buttonSearch.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(MainFrame.tab==2) {
+						if(!textField.getText().equals("") ) {	
+										if(textField.getText().contains(":")) {
+												String[] kolone=textField.getText().split(";");
+												String nazivKolone;
+												String tekst=null;
+												@SuppressWarnings("unchecked")
+												TableRowSorter<TableModel> sorter = (TableRowSorter<TableModel>)  MainFrame.getInstance().getTabelaPredmeta().getRowSorter();
+												List<RowFilter<Object,Object>> filters = new ArrayList<RowFilter<Object,Object>>(2);
+												for(int i=0;i<kolone.length;i++) {
+													String[] podela2=kolone[i].split(":");
+													if(podela2.length==2 ) {		
+														nazivKolone=podela2[0];
+														tekst=podela2[1];
+														AbstractTableModelPredmeti atmp=new AbstractTableModelPredmeti();
+														String sifraPredmeta=null;
+														String nazivPredmeta=null;
+														String semestar=null;
+														String godinaIzvodjenja=null;
+														String predmetniProfesor=null;
+														
+														if(nazivKolone.toLowerCase().equals(atmp.getColumnName(0).toLowerCase())){
+															sifraPredmeta=tekst.toUpperCase();
+																													
+															  filters.add(RowFilter.regexFilter(sifraPredmeta,0));
+													          													
+														}else if (nazivKolone.toLowerCase().equals(atmp.getColumnName(1).toLowerCase())) {
+															nazivPredmeta=tekst.substring(0, 1).toUpperCase();
+					
+															  filters.add(RowFilter.regexFilter(nazivPredmeta,1));
+													          								           
+														}else if (nazivKolone.toLowerCase().equals(atmp.getColumnName(2).toLowerCase())) {
+															semestar=tekst;
+															
+															 filters.add(RowFilter.regexFilter(semestar,2));
+															
+														}else if (nazivKolone.toLowerCase().equals(atmp.getColumnName(3).toLowerCase())) {
+															godinaIzvodjenja=tekst;
+															
+															 filters.add(RowFilter.regexFilter(godinaIzvodjenja,3));
+															 
+														}else if (nazivKolone.toLowerCase().equals(atmp.getColumnName(4).toLowerCase())) {
+																predmetniProfesor=tekst.substring(0,1).toUpperCase();
+																											
+																 filters.add(RowFilter.regexFilter(predmetniProfesor,4));
+																									
+														}else {
+															JOptionPane.showMessageDialog(null, "Ne postoji kolona " + nazivKolone + " u tabeli predmeta", "Greska",0);
+															break;
+														}
+																
+													
+													
+													
+													sorter = new TableRowSorter<TableModel>( MainFrame.getInstance().getTabelaPredmeta().getModel());
+												    sorter.setRowFilter(RowFilter.andFilter(filters));
+								
+												    
+													 MainFrame.getInstance().getTabelaPredmeta().setRowSorter(sorter);
+													 textField.setText("");
+														
+														
+														}
+												}
+										}
+										
+						}else {
+							
+							 MainFrame.getInstance().getTabelaPredmeta().setModel(new AbstractTableModelPredmeti());
+							 MainFrame.getInstance().azurirajTabeluPredmeta();
+								
+								
+							
+								
+							}
+				}
+			}	
+		});
 		
 		add(buttonSearch);
 		
