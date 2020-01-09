cddfb21fab7463ac8d4f0597efe12b7416aa15e1
Nemanja Jevtic <njevtic998@gmail.com>
2020-01-10 00:23:45 +0100

    #dorada_DijalogHelp

--- src/dialozi/DijalogHelp.java
+++ src/dialozi/DijalogHelp.java
@@ -17,7 +17,7 @@ import javax.swing.JPanel;
 public class DijalogHelp {
 
 	 public DijalogHelp() {
-		 JDialog  dijalogHelp = new ModalniDijalog(new JFrame(), "Pomoc i nacin koriscenja aplikacije", true,800,500);
+		 JDialog  dijalogHelp = new ModalniDijalog(new JFrame(), "Pomoc i nacin koriscenja aplikacije", true,1200,700);
 		 
 		 JLabel statusBar = new JLabel();
 		 dijalogHelp.add(statusBar,BorderLayout.SOUTH);
@@ -36,21 +36,135 @@ public class DijalogHelp {
 		JLabel brisanjeEntiteta = new JLabel("Klikom na ikonicu omogucuje se brisanje postojeceg entiteta samo ako je red selektovan");
 		JLabel brisanjeIkonica = new JLabel(new ImageIcon("images/delete.jpg"));
 		JLabel precice = new JLabel("Precice");
+		JLabel ctrlC = new JLabel("CTRL+C");
+		JLabel ctrlI = new JLabel("CTRL+I");
 		JLabel opisKoriscenja = new JLabel("Kratak opis koriscenja ikonica");
 		JLabel ikonice = new JLabel("Ikonice");
+		JLabel closeOpis = new JLabel("Klikom na ikonicu zatvara se program i vrsi se cuvanje podataka");
+		JLabel infOpis = new JLabel("Klikom na ikonicu pruza se biografija autora");
+		JLabel dsnpOpis = new JLabel("Klikom na ikonicu omogucava se dodavanje studenta na oznaceni predmet");
+		JLabel dpnpOpis = new JLabel("Klikom na ikonicu omogucava se dodavanje profesora na oznaceni predmet");
+		JLabel lupaOpis = new JLabel("Pretraga primer - ime:*****;prezime:*****;....");
+		JLabel colse = new JLabel(new ImageIcon("images/xIcon.jpg"));
+		JLabel info = new JLabel(new ImageIcon("images/aboutIcon.jpg"));
+		JLabel dsnp = new JLabel(new ImageIcon("images/add.jpg"));
+		JLabel dpnp = new JLabel(new ImageIcon("images/profesor.jpg"));
+		JLabel lupa = new JLabel(new ImageIcon("images/pretraga.jpg"));
 		JLabel ctrlN = new JLabel("CTRL+N");
 		JLabel ctrlE = new JLabel("CTRL+E");
 		JLabel ctrlD = new JLabel("CTRL+D");
 		
+		JLabel izmenaDodavanje = new JLabel("Ogranicenja pri dodavanju/izmeni");
+		
+		JLabel Student = new JLabel("STUDENT");
+		JLabel indeksOgranicenje = new JLabel("Ocekivani format indeksa : XXBROJ/GODINAUPISA");
+		JLabel datumOgranicenje = new JLabel("Ocekivani format za datume : yyyy.MM.dd.");
+		JLabel adresaOgranicenje = new JLabel("Ocekivani format za adresu : Ulica [broj] , grad");
+		JLabel brojTelefonaOgranicenje = new JLabel("Ocekivani format za broj tel : pb/****-***");
+		
+		JLabel Profesor = new JLabel("PROFESOR");
+		JLabel datumOgranicenje2 = new JLabel("Ocekivani format za datume : yyyy.MM.dd.");
+		JLabel adresaOgranicenje2 = new JLabel("Ocekivani format za adresu : Ulica [broj] , grad");
+		JLabel brojTelefonaOgranicenje2 = new JLabel("Ocekivani format za broj tel : pb/****-***");
+		JLabel adresaOgranicenje3 = new JLabel("Ocekivani format za adresu : Ulica [broj] , grad , kancelarija");
+		JLabel brLicneOgranicenje = new JLabel("Ocekivani format licne karte je 9-tocifreni broj");
+		
+		JLabel Predmet = new JLabel("PREDMET");
+		JLabel sifraOgranicenje = new JLabel("Sifra predmeta se sastoji od velikog slova");
+		JLabel ogranicenjeNastavak = new JLabel("celobrojnog broja i moze da sadrzi i karakter -");
+		JLabel ogranicenjeSemestra = new JLabel("Voditi racuna o pravilnom izboru semestra");
+		JLabel ogranicenjeNastavak2 = new JLabel("i odgovarajucoj godini studija za taj semestar");
+		
 		Font font = new Font("Serif", Font.BOLD, 20);
+		Font font2 = new Font("Serif", Font.BOLD, 15);
 		precice.setFont(font);
 		precice.setForeground(Color.RED);
 		opisKoriscenja.setFont(font);
 		opisKoriscenja.setForeground(Color.RED);
 		ikonice.setFont(font);
 		ikonice.setForeground(Color.RED);
+		izmenaDodavanje.setFont(font);
+		izmenaDodavanje.setForeground(Color.RED);
+		Student.setFont(font2);
+		Student.setForeground(Color.RED);
+		Profesor.setFont(font2);
+		Profesor.setForeground(Color.RED);
+		Predmet.setFont(font2);
+		Predmet.setForeground(Color.RED);
 		
 		GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.insets = new Insets(10, 0, 10, 20);
+		gbc.gridx = 1;
+		gbc.gridy = 9;
+		panel.add(izmenaDodavanje,gbc);
+		
+		gbc.insets = new Insets(5, 0, 5, 20);
+		gbc.gridx = 0;
+		gbc.gridy = 10;
+		panel.add(Student,gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 11;
+		panel.add(indeksOgranicenje,gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 12;
+		panel.add(datumOgranicenje,gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 13;
+		panel.add(adresaOgranicenje,gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 14;
+		panel.add(brojTelefonaOgranicenje,gbc);
+		
+		gbc.insets = new Insets(5, 0, 5, 20);
+		gbc.gridx = 1;
+		gbc.gridy = 10;
+		panel.add(Profesor,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 11;
+		panel.add(datumOgranicenje2,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 12;
+		panel.add(adresaOgranicenje2,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 13;
+		panel.add(brojTelefonaOgranicenje2,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 14;
+		panel.add(adresaOgranicenje3,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 15;
+		panel.add(brLicneOgranicenje,gbc);
+		
+		gbc.insets = new Insets(5, 0, 5, 20);
+		gbc.gridx = 2;
+		gbc.gridy = 10;
+		panel.add(Predmet,gbc);
+		
+		gbc.gridx = 2;
+		gbc.gridy = 11;
+		panel.add(sifraOgranicenje,gbc);
+		
+		gbc.gridx = 2;
+		gbc.gridy = 12;
+		panel.add(ogranicenjeNastavak,gbc);
+		
+		gbc.gridx = 2;
+		gbc.gridy = 13;
+		panel.add(ogranicenjeSemestra,gbc);
+		
+		gbc.gridx = 2;
+		gbc.gridy = 14;
+		panel.add(ogranicenjeNastavak2,gbc);
 		
 		gbc.insets = new Insets(5, 0, 5, 20);
 		gbc.gridx = 0;
@@ -69,6 +183,26 @@ public class DijalogHelp {
 		gbc.gridy = 3;
 		panel.add(brisanjeIkonica,gbc);
 		
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		panel.add(colse,gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		panel.add(info,gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 6;
+		panel.add(dsnp,gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 7;
+		panel.add(dpnp,gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 8;
+		panel.add(lupa,gbc);
+		
 		gbc.insets = new Insets(5, 20, 5, 20);
 		gbc.gridx = 1;
 		gbc.gridy = 0;
@@ -86,6 +220,26 @@ public class DijalogHelp {
 		gbc.gridy = 3;
 		panel.add(brisanjeEntiteta,gbc);
 		
+		gbc.gridx = 1;
+		gbc.gridy = 4;
+		panel.add(closeOpis,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 5;
+		panel.add(infOpis,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 6;
+		panel.add(dsnpOpis,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 7;
+		panel.add(dpnpOpis,gbc);
+		
+		gbc.gridx = 1;
+		gbc.gridy = 8;
+		panel.add(lupaOpis,gbc);
+		
 		gbc.insets = new Insets(5, 20, 5, 0);
 		gbc.gridx = 2;
 		gbc.gridy = 0;
@@ -102,6 +256,15 @@ public class DijalogHelp {
 		gbc.gridx = 2;
 		gbc.gridy = 3;
 		panel.add(ctrlD,gbc);
+		
+		gbc.gridx = 2;
+		gbc.gridy = 4;
+		panel.add(ctrlC,gbc);
+		
+		gbc.gridx = 2;
+		gbc.gridy = 5;
+		panel.add(ctrlI,gbc);
+		
 		
 		 dijalogHelp.setVisible(true);
 	}
